8ab0871b00f4b47e1869e953ca29a2cf728129da
sanchitmirg <sanchitmirg0@gmail.com>
2017-08-29 22:54:08 +0530

    First Commit

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "app/recipes/recipe.model";
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+  finalRecipe: Recipe;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  selectedRecipe(recipe){
+
+  }
+}
